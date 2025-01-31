c425b01e030f4ebf1ce4471323436d72568ad1bb
Muhamad Ramdhani Akbar <ramdhaniakbarmuhamad@gmail.com>
2025-02-01 03:49:46 +0700

    feat: update storage link and update profile

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class UserController extends Controller
+{
+    public function user_profile(Request $request)
+    {
+        $token = $request->user()->currentAccessToken();
+        if (!$token) {
+            return response()->json(['message' => 'No active token found'], 404);
+        }
+
+        return response()->json([
+            'message' => 'User profile',
+            'data' => [
+                'user' => Auth::user(),
+            ]
+        ]);
+    }
+
+    public function update_profile(Request $request)
+    {
+        // validate request
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,' . Auth::id(),
+            'phone' => 'nullable|string|max:20',
+            'photo' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg',
+            'date_of_birth' => 'nullable|date',
+            'gender' => 'nullable|in:male,female,other'
+        ]);
+
+        // get user
+        $user = User::find(Auth::id());
+
+        // update user
+        $user->name = $request->name;
+        $user->email = $request->email;
+        
+        // update additional columns
+        $user->phone = $request->phone;
+        $user->date_of_birth = $request->date_of_birth;
+        $user->gender = $request->gender;
+        $user->address = $request->address;
+
+        if ($request->old_password || $request->password) {
+            $request->validate([
+                'old_password' => 'required|string',
+                'password' => 'required|string|min:8|confirmed',
+            ]);
+
+            if (!Hash::check($request->old_password, $user->password)) {
+                return response()->json(['message' => 'Old password is incorrect'], 401);
+            } else if (password_verify($request->password, $user->password)) {
+                return response()->json(['message' => 'New password must be different from old password'], 400);
+            } else if (password_verify($request->old_password, $user->password)) {
+                $user->password = Hash::make($request->password);
+            }
+        }
+
+        if ($request->hasFile('photo')) {
+            // check if old photo exists
+            if ($user->photo && Storage::disk('public')->exists('images/user/' . $user->photo)) {
+                // delete old photo
+                Storage::disk('public')->delete('images/user/' . $user->photo);
+            }
+
+            // upload new photo
+            $photo = $request->file('photo');
+            $photo_name = Str::random(10) . '.' . $photo->getClientOriginalExtension();
+            $photo->storeAs('images/user', $photo_name, 'public');
+            $user->photo = $photo_name;
+        }
+
+        $user->save();
+
+        return response()->json([
+            'message' => 'User profile updated successfully',
+            'data' => [
+                'user' => $user,
+            ]
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,8 +14,7 @@ Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [UserController::class, 'user_profile'])->name('user.profile');
+    Route::put('/user', [UserController::class, 'update_profile'])->name('user.update');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
