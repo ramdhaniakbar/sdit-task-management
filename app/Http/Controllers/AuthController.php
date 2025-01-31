a6ea25fcbc40fc7ddc548ddd92ce4ae78834204f
Muhamad Ramdhani Akbar <ramdhaniakbarmuhamad@gmail.com>
2025-02-01 01:59:24 +0700

    feat: update laravel sanctum and auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        // validate request
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        // create new user
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // generate token expires in 7 days
+        $token = $user->createToken('auth_token')->expiresAt(Carbon::now()->addDays(7));
+
+        return response()->json([
+            'message' => 'User registered successfully',
+            'data' => [
+                'user' => $user,
+                'access_token' => $token->plainTextToken,
+                'token_type' => 'Bearer',
+                'expires_at' => Carbon::now()->addDays(7)->toDateTimeString(),
+            ]
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        // validate request
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        // get user
+        $user = User::where('email', $request->email)->first();
+        
+        // check user and password
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        // generate token expires in 7 days
+        $token = $user->createToken('auth_token')->expiresAt(Carbon::now()->addDays(7));
+
+        return response()->json([
+            'message' => 'User logged in successfully',
+            'data' => [
+                'user' => $user,
+                'access_token' => $token->plainTextToken,
+                'token_type' => 'Bearer',
+                'expires_at' => Carbon::now()->addDays(7)->toDateTimeString(),
+            ],
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ], 200);
+    }
+}
